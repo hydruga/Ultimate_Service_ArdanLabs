cebb97ff6c2d1e7f4834c8ae81c7d2e128502fb5
xenopheon <xenopheon@xenopheons-MBP.attlocal.net>
2022-11-07 15:22:35 -0500

    Added custom error handling and validations

--- app/services/sales-api/handlers/v1/testgrp/testgrp.go
+++ app/services/sales-api/handlers/v1/testgrp/testgrp.go
@@ -2,8 +2,11 @@ package testgrp
 
 import (
 	"context"
+	"errors"
+	"math/rand"
 	"net/http"
 
+	"github.com/hydruga/ultimate_service/app/business/sys/validate"
 	"github.com/hydruga/ultimate_service/app/foundation/web"
 	"go.uber.org/zap"
 )
@@ -14,6 +17,12 @@ type Handlers struct {
 
 // Test handler for development.
 func (h Handlers) Test(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+	if n := rand.Intn(100); n%2 == 0 {
+		//return errors.New("untrusted errors")
+		//return web.NewShutdownError("restart service")
+		return validate.NewRequestError(errors.New("trusted error"), http.StatusBadRequest)
+	}
+
 	status := struct {
 		Status string
 	}{
